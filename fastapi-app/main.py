3c76b011a83e5dc7828001682e48f848f3bcb043
Raymond <42894676+SameriteRL@users.noreply.github.com>
2025-02-16 02:01:34 -0500

    stream endpoint now correctly gets video segments

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,14 +1,18 @@
 import os
+import re
 import subprocess
 
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import Response
+from fastapi.staticfiles import StaticFiles
 
 _UPLOAD_DIR = "uploads"
 _STATIC_DIR = "static"
+_HLS_DIR = os.path.join(_STATIC_DIR, "hls")
 os.makedirs(_UPLOAD_DIR, exist_ok=True)
 os.makedirs(_STATIC_DIR, exist_ok=True)
+os.makedirs(_HLS_DIR, exist_ok=True)
 
 
 app = FastAPI()
@@ -19,17 +23,14 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
+app.mount("/hls", StaticFiles(directory=_HLS_DIR), name="hls")
 
 
-@app.post("/upload")
-def upload_video(file: UploadFile):
-    buf_location = os.path.join(_UPLOAD_DIR, file.filename)
-    with open(buf_location, "wb") as buf:
-        buf.write(file.file.read())
-    cmd = [
+def _get_ffmpeg_command(input_file: str, output_manifest_file: str) -> list[str]:
+    return [
         "ffmpeg",
         "-i",
-        buf_location,
+        input_file,
         "-profile:v",
         "baseline",  # Broad compatibility
         "-level",
@@ -42,8 +43,23 @@ def upload_video(file: UploadFile):
         "0",  # Keep all segments in the manifest
         "-f",
         "hls",
-        os.path.join(_STATIC_DIR, "playlist.m3u8"),
+        output_manifest_file,
     ]
+
+
+def _clear_directory(dir: str) -> None:
+    for file in os.listdir(dir):
+        os.remove(os.path.join(dir, file))
+
+
+@app.post("/upload")
+def upload_video(file: UploadFile):
+    _clear_directory(_UPLOAD_DIR)
+    _clear_directory(_HLS_DIR)
+    buf_location = os.path.join(_UPLOAD_DIR, file.filename)
+    with open(buf_location, "wb") as buf:
+        buf.write(file.file.read())
+    cmd = _get_ffmpeg_command(buf_location, os.path.join(_HLS_DIR, "playlist.m3u8"))
     result = subprocess.run(cmd, capture_output=True, text=True)
     if result.returncode != 0:
         raise HTTPException(status_code=500, detail=f"FFmpeg failed: {result.stderr}")
@@ -51,5 +67,12 @@ def upload_video(file: UploadFile):
 
 
 @app.get("/stream")
-def play_video():
-    return FileResponse(os.path.join(_STATIC_DIR, "playlist.m3u8"))
+def get_manifest():
+    video_segment_regex = re.compile(r"(.*.ts)\n")
+    manifest_content = None
+    with open(os.path.join(_HLS_DIR, "playlist.m3u8"), "r") as manifest:
+        manifest_content = manifest.read()
+    manifest_content = re.sub(video_segment_regex, "hls/\\1\n", manifest_content)
+    return Response(
+        content=manifest_content, media_type="application/vnd.apple.mpegurl"
+    )
