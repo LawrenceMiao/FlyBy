4eb2e9a3d32c13d6ef8406c1a4c69d55edc5d6aa
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-02-15 19:37:58 -0500

    shadcn and magic ui

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import { BentoDemo } from "./pages/Demo";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
+          <Route path="/demo" element={<BentoDemo />}></Route>
         </Routes>
       </Router>
     </>
