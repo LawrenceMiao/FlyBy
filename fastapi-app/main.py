3860cb07ffdc7e86262fc7a4e8c81ebcc58cca85
Raymond <42894676+SameriteRL@users.noreply.github.com>
2025-02-16 00:21:21 -0500

    add upload and stream endpoints

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+import os
+import subprocess
+
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+_UPLOAD_DIR = "uploads"
+_STATIC_DIR = "static"
+os.makedirs(_UPLOAD_DIR, exist_ok=True)
+os.makedirs(_STATIC_DIR, exist_ok=True)
+
 
 app = FastAPI()
 app.add_middleware(
@@ -11,6 +20,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def get():
-    return {"message": "Hello World"}
+
+@app.post("/upload")
+def upload_video(file: UploadFile):
+    buf_location = os.path.join(_UPLOAD_DIR, file.filename)
+    with open(buf_location, "wb") as buf:
+        buf.write(file.file.read())
+    cmd = [
+        "ffmpeg",
+        "-i",
+        buf_location,
+        "-profile:v",
+        "baseline",  # Broad compatibility
+        "-level",
+        "3.0",
+        "-start_number",
+        "0",  # Start segment index at 0
+        "-hls_time",
+        "10",  # 10-second segments
+        "-hls_list_size",
+        "0",  # Keep all segments in the manifest
+        "-f",
+        "hls",
+        os.path.join(_STATIC_DIR, "playlist.m3u8"),
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise HTTPException(status_code=500, detail=f"FFmpeg failed: {result.stderr}")
+    return 200
+
+
+@app.get("/stream")
+def play_video():
+    return FileResponse(os.path.join(_STATIC_DIR, "playlist.m3u8"))
